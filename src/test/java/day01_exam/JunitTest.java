b875b35f6d4660dfe527da61358b8684409c0a85
yuanbaowang <2637562166@qq.com>
2019-12-04 14:39:21 +0800

    chu shi hua day01_exam

---
+++ src/test/java/day01_exam/JunitTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package day01_exam;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+/**
+ * @author 袁保旺
+ *
+ * 2019年12月2日 下午2:08:10 
+ */
+public class JunitTest {
+	
+	@Test
+	public void day01() {
+		
+	}
+	
+	@Test
+	public void day02() {
+		//设置断言
+		Assert.assertFalse(1==2);
+	}
+
+}
